eb71819aa368fb8e97740dcea3753e05b80864ce
Keisuke SATO <riaf@me.com>
2012-02-13 10:43:59 +0900

    remove phpdoc

---
+++ docinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# phpmanual
+mkdir -p ./.doc/php
+mkdir -p /tmp/phpdoc
+
+curl "http://jp.php.net/distributions/manual/php_manual_ja.tar.gz" | tar zxf - -C /tmp/phpdoc
+mv /tmp/phpdoc/php-chunked-xhtml/* .doc/php
+rm -rf /tmp/phpdoc
+
