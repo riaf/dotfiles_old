c2dbac407b54c45c5a9631f31e87df3f9634179d
Keisuke SATO <riaf@me.com>
2012-02-13 10:46:49 +0900

    こうですね

--- docinstall.sh
+++ docinstall.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # phpmanual
-mkdir -p ./.doc/php
+mkdir -p .doc
+rm -rf .doc/php
 mkdir -p /tmp/phpdoc
 
 curl "http://jp.php.net/distributions/manual/php_manual_ja.tar.gz" | tar zxf - -C /tmp/phpdoc
-mv /tmp/phpdoc/php-chunked-xhtml/* .doc/php
+mv /tmp/phpdoc/php-chunked-xhtml .doc/php
 rm -rf /tmp/phpdoc
 
